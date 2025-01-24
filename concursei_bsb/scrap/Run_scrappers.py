3a9fc51bb96e589a0a6d338e6370458de99bdc41
ArturDCR <arturdecamargos@gmail.com>
2025-01-23 22:46:46 -0300

    Atualização do Scrap para encontrar concurso e suas vagas

--- concursei_bsb/scrap/Run_scrappers.py
+++ concursei_bsb/scrap/Run_scrappers.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import time
 from bs4 import BeautifulSoup as BSP
 
 class Run_scrappers():
@@ -8,6 +9,7 @@ class Run_scrappers():
     
     def pageRequest(self, url: str):
         try:
+            time.sleep(5)
             return requests.get(url)
         except requests.HTTPError:
             print("An http error has ocurred, process has exited")
@@ -27,22 +29,42 @@ class Run_scrappers():
 
     def get_contests_links(self, soup : BSP):
     
-        constests = []
+        constests = {
+            'concurso': [],
+            'vagas': [],
+            # 'remuneracao': [],
+            # 'inscricao': []
+        }
     
-        links = soup.select("td > a")
-        
-        for link in links:
-            constests.append(link['href'])
-    
+        for i in range(len(soup.select(".center"))):
+            constests['concurso'].append(soup.select(".center")[i].text)
+            constests['vagas'].append(soup.select("td > a")[i].text) 
+
         return constests
     
-    def get_info_json(self, soup: BSP):
+    # def get_info_json(self, soup: BSP):
 
-        for indice in range(1,7):
-            print(soup.find_all(f'h{indice}'))
+    #     for indice in range(1,7):
+    #         print(soup.find_all(f'h{indice}'))
+        
+        #Bloco de peqsuisa usando a regex passada dentro dos requests
+        #regex = re.compile(r'\b(\d+\s+cargos|cargos\s+\d+)\b|\b(\d+\s+vagas|vagas\s+\d+)\b', re.IGNORECASE)
+        # # Buscar todas as tags
+        # tags = soup.find_all()
+
+        # # Aplicar a regex diretamente no texto das tags e capturar as correspondências
+        # resultados = []
+        # for tag in tags:
+        #     if tag.string:  # Verifica se a tag possui conteúdo de texto
+        #         matches = regex.findall(tag.string)
+        #         resultados.extend(matches)
+
+        # for resultado in resultados:
+        #     print(resultado)
  
 if __name__ == "__main__":
     scrap = Run_scrappers()
     soup = scrap.initWebScraper('https://concursosnobrasil.com/concursos/df')
-    for contest in scrap.get_contests_links(soup):
-        scrap.get_info_json(scrap.initWebScraper(f'{contest}'))
+    scrap.get_contests_links(soup)
+    # for contest in scrap.get_contests_links(soup):
+    #     scrap.get_info_json(scrap.initWebScraper(f'{contest}'))
