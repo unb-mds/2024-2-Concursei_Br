d0d6447a0dc31a1290a4674712d06a1d44b31bae
Jose <josevictor21112005@gmail.com>
2025-01-25 12:56:25 -0300

    Revert "Filtrando links para scraping"
    This reverts commit 26174bebe0c691ae80c65cf06528269476ae6834.
    
    Desfazendo commit indevido

--- concursei_bsb/scrap/Run_scrappers.py
+++ concursei_bsb/scrap/Run_scrappers.py
@@ -5,20 +5,12 @@ from bs4 import BeautifulSoup as BSP
 
 class Run_scrappers():
     def __init__(self):
-
-        self.__constests = {
-            'concursos': [],
-            'vagas': [],
-            'links': []
-            # 'remuneracao': [],
-            # 'inscricao': []
-        }
+        pass
     
     def pageRequest(self, url: str):
         try:
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
             time.sleep(5)
-            return requests.get(url, headers=headers)
+            return requests.get(url)
         except requests.HTTPError:
             print("An http error has ocurred, process has exited")
             return None
@@ -35,53 +27,44 @@ class Run_scrappers():
     
         return BSP(webResponse.text, parser)
 
-
-    def get_links(self, soup : BSP):
-
-        links = []
-
-        constest_names = soup.select(".center")
-        vagas = soup.select("td > a")
-
-        for i in range(len(soup.select(".center"))):
-            self.__constests['concursos'].append(constest_names[i])
-            self.__constests['vagas'].append(vagas[i].text) 
-            links.append(vagas[i]['href'])
-
-        return links
+    def get_contests_links(self, soup : BSP):
     
-    def search_web_info(self, links_array : list):
-        
-        for link in links_array:
-            soup = self.initWebScraper(link)
+        constests = {
+            'concurso': [],
+            'vagas': [],
+            # 'remuneracao': [],
+            # 'inscricao': []
+        }
+    
+        for i in range(len(soup.select(".center"))):
+            constests['concurso'].append(soup.select(".center")[i].text)
+            constests['vagas'].append(soup.select("td > a")[i].text) 
 
-            # Tratamento aqui
-            print(soup)
-
-
+        return constests
+    
+    # def get_info_json(self, soup: BSP):
 
     #     for indice in range(1,7):
     #         print(soup.find_all(f'h{indice}'))
         
-    #     Bloco de peqsuisa usando a regex passada dentro dos requests
-    #     regex = re.compile(r'\b(\d+\s+cargos|cargos\s+\d+)\b|\b(\d+\s+vagas|vagas\s+\d+)\b', re.IGNORECASE)
-    #     # Buscar todas as tags
-    #     tags = soup.find_all()
+        #Bloco de peqsuisa usando a regex passada dentro dos requests
+        #regex = re.compile(r'\b(\d+\s+cargos|cargos\s+\d+)\b|\b(\d+\s+vagas|vagas\s+\d+)\b', re.IGNORECASE)
+        # # Buscar todas as tags
+        # tags = soup.find_all()
 
-    #     # Aplicar a regex diretamente no texto das tags e capturar as correspondências
-    #     resultados = []
-    #     for tag in tags:
-    #         if tag.string:  # Verifica se a tag possui conteúdo de texto
-    #             matches = regex.findall(tag.string)
-    #             resultados.extend(matches)
+        # # Aplicar a regex diretamente no texto das tags e capturar as correspondências
+        # resultados = []
+        # for tag in tags:
+        #     if tag.string:  # Verifica se a tag possui conteúdo de texto
+        #         matches = regex.findall(tag.string)
+        #         resultados.extend(matches)
 
-    #     for resultado in resultados:
-    #         print(resultado)
+        # for resultado in resultados:
+        #     print(resultado)
  
 if __name__ == "__main__":
     scrap = Run_scrappers()
     soup = scrap.initWebScraper('https://concursosnobrasil.com/concursos/df')
-    links = scrap.get_links(soup)
-    scrap.search_web_info(links)
-
-        
+    scrap.get_contests_links(soup)
+    # for contest in scrap.get_contests_links(soup):
+    #     scrap.get_info_json(scrap.initWebScraper(f'{contest}'))
