6a97f94c3ca6d637a511c9a818fbd563eb345bd5
André Meyer <andre.dedemeyer@gmail.com>
2025-02-07 16:39:07 -0300

    FILTRO UF

--- concursei_bsb/app/pages/2_Editais.py
+++ concursei_bsb/app/pages/2_Editais.py
@@ -4,7 +4,7 @@ Created on Mon Jan 20 19:12:31 2025
 
 @author: Marqu
 """
-
+import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title="Editais e Provas", page_icon="📄", layout="wide")
@@ -28,3 +28,21 @@ st.markdown(
 st.title("Editais e Provas")
 st.write("### Links para Editais e Provas Anteriores")
 st.write("Nesta página você terá acesso direto aos editais e provas anteriores para download e consulta.")
+
+df = pd.read_csv("../data/contests_info.csv", sep=';')
+
+def filtro_uf():
+    regioes = df['Região'].unique()
+    regiao_selecionada = st.multiselect("Selecione a Região:", options=regioes)
+    if regiao_selecionada:
+        # Filtrar o DataFrame com base na seleção do usuário
+        df_filtrado = df[df['Região'].isin(regiao_selecionada)]
+        
+        # Exibir o DataFrame filtrado
+        st.write(df_filtrado)
+    else:
+        # Mensagem quando nenhuma região for selecionada
+        st.write("Selecione uma ou mais regiões para visualizar os dados.")
+
+
+filtro_uf()
